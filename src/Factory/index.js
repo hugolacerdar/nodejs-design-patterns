7a3f00788503226b3d75fa937ef0ee3e369a8127
root <hugolacerdar@gmail.com>
2022-01-21 15:45:50 -0300

    Align comment

--- src/Factory/index.js
+++ src/Factory/index.js
@@ -1,5 +1,6 @@
 /* 
-- Intent: "Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses." 
+- Intent: "Define an interface for creating an object, but let subclasses decide which 
+class to instantiate. Factory Method lets a class defer instantiation to subclasses." 
 */
 
 var userFactory = require("./userFactory");
